32e828e93ec52644cfd3a0920ecee45c2112921c
DannyUR <111530669+DannyUR@users.noreply.github.com>
2025-08-27 16:25:57 -0600

    modificacion a la base de datos
    creacion de la tabla post

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 Route::get('/contacto', function (){
     return view('contacto');
 });
+Route::get('/about', function (){
+    return view('about');
+});
+Route::get('/post', function (){
+    return view('post');
+});
